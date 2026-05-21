069c8b4734a9582722fc84096023ede9e5a173b0
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-22 01:28:35 +0600

    feat: connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI
 const port = 5555
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db('pawsome_db')
+        const petsCollection = db.createCollection('pets')
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('pawsome-server is running!');
